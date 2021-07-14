c4b201dba833897303c4ad667f1657edb9ce570e
Grant <grantyli94@gmail.com>
2021-07-14 15:31:41 -0400

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,15 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/users')
+
+@app.route('users/new')
+
+@app.route('users/new', method=["POST"])
+
+
--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 """Models for Blogly."""
+from typing import Text
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False)
+    last_name = db.Column(db.String(50),
+                           nullable=False)
+    image_url = db.Column(db.String)
+    """ TODO: Why can't we set default text value? """
+
